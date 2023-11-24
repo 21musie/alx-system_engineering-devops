a02993f1bcc944ad387eee8869cea223fdff5210
21musie <104737346+21musie@users.noreply.github.com>
2023-11-25 01:07:26 +0300

    adding require on python3-pip = 'installed'

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,14 @@
 # Install flask 
-exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
+package { 'python3-pip':
+  ensure => installed,
 }
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require => Exec['apt-get update']
+package { 'python3-dev':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => [Package['python3-pip'], Package['python3-dev']],
 }
