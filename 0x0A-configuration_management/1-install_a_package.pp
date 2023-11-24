23f4a9d08a3f0f5f5c23baa90feb259709bedf93
21musie <104737346+21musie@users.noreply.github.com>
2023-11-25 01:23:25 +0300

    check all the requires
    for python3-pip and python3-dev and build-essentials

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,18 @@
 # Install flask 
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'python3-dev':
+  ensure => installed,
+}
+
+package { 'build-essential':
+  ensure => installed,
+}
+
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => [Package['python3-pip'], Package['python3-dev'], Package['build-essential']],
 }
