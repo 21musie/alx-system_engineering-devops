90a9adcd7ae95eb007e1b48acd53a00c8498f518
21musie <104737346+21musie@users.noreply.github.com>
2023-11-25 01:03:37 +0300

    check if apt-get update works fine

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Install flask 
+exec { 'apt-get update':
+    command => '/usr/bin/apt-get update'
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require => Exec['apt-get update']
 }
