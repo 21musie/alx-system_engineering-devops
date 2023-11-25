f186af3bc36d3bdff131fd2f4a954ac3eeff5e48
21musie <104737346+21musie@users.noreply.github.com>
2023-11-25 13:29:15 +0300

    Update 1-install_a_package.pp
    change exec to package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,5 @@
 # Install flask 
-package { 'python3-pip':
-  ensure => installed,
-}
-
-package { 'python3-dev':
-  ensure => installed,
-}
-
-package { 'build-essential':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  require => [Package['python3-pip'], Package['python3-dev'], Package['build-essential']],
+package {'flask':
+  ensure   => installed,
+  provider => 'pip3',
 }
